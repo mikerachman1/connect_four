5b7c832840933a8c8685a5aad161193c25287087
Mike Rachman <mikerachman1@gmail.com>
2022-07-05 09:16:20 -0400

    Added tests and methods: verify_input, update_board, and change_turn

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 class ConnectFour
+  attr_accessor :board, :turn
+
+  def initialize
+    @board = Array.new(7) { Array.new }
+    @turn = 2
+  end
 
   def player_input
     loop do
@@ -8,5 +14,22 @@ class ConnectFour
       puts 'Input error! Enter a number between 1 and 7.'
     end
   end
-  
+
+  def verify_input(row)
+    board[(row - 1)].length == 6 ? nil : row
+  end
+
+  def update_board(row)
+    turn == 1 ? piece = 'X' : piece = 'O'
+    board[(row - 1)] << piece
+    board
+  end
+
+  def change_turn
+    turn == 1 ? @turn = 2 : @turn = 1
+  end
 end
+
+ game = ConnectFour.new
+ game.change_turn
+ p game.turn
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -51,4 +51,90 @@ describe ConnectFour do
         end
       end
   end
+
+  describe '#update_board' do
+
+    subject(:game_board) { described_class.new }
+
+    context 'First player move in empty slot' do
+
+      before do
+        game_board.instance_variable_set(:@turn, 1)
+      end
+    
+      it 'Updates game board array accordingly' do
+        row = 1
+        board = game_board.board
+        expect { game_board.update_board(row) }.to change { board[0].length }.by(1) 
+      end
+    end
+
+    context 'Move into slot with other pieces' do
+
+      before do
+        game_board.instance_variable_set(:@turn, 2)
+        game_board.instance_variable_set(:@board, [[], ['X', 'X'], [], [], [], [], []])
+      end
+      
+      it 'Updates game board array accordingly' do
+        row = 2
+        board = game_board.board
+        game_board.update_board(row)
+        expect(board[1][2]).to eq('O') 
+      end 
+    end
+  end
+
+  describe '#verify_input' do
+
+    subject(:game_verify) { described_class.new }
+
+    context 'valid move, empty spaces in row' do
+      it 'returns input' do
+        row = 1
+        expect(game_verify.verify_input(row)).to eq(row)
+      end
+    end
+
+    context 'invalid move, spaces full in row' do
+
+      before do
+        game_verify.instance_variable_set(:@board, [['X', 'O', 'X', 'O', 'X', 'O'], [], [], [], [], [], []])
+      end
+
+      it 'returns nil' do
+        row = 1
+        expect(game_verify.verify_input(row)).to be_nil
+      end
+    end
+  end
+
+  describe '#change_turn' do
+
+    subject(:game_change) { described_class.new }
+
+    context 'Was P1 turn last' do
+
+      before do
+        game_change.instance_variable_set(:@turn, 1)
+      end
+
+      it 'changes to P2 turn' do
+        game_change.change_turn
+        expect(game_change.turn).to eq(2)
+      end
+    end
+
+    context 'Was P2 turn last' do
+
+      before do
+        game_change.instance_variable_set(:@turn, 2)
+      end
+
+      it 'changes to P1 turn' do
+        game_change.change_turn
+        expect(game_change.turn).to eq(1)
+      end
+    end
+  end
 end
